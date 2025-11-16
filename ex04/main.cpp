ae0be0be7ee2a07464905e2df9361c0d24c0ecb3
Ecioq <siebedeboeck@hotmail.com>
2025-11-16 13:32:47 +0100

    Update main.cpp

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: side-boe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 14:53:03 by side-boe          #+#    #+#             */
-/*   Updated: 2025/11/13 14:53:03 by side-boe         ###   ########.fr       */
+/*   Updated: 2025/11/16 13:26:51 by side-boe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "file.hpp"
@@ -17,11 +17,19 @@ int main(int argc, char **argv)
 	File f;
 	if (argc != 4)
 	{
-	std::cout << "you have to input filename than the string you want to find and only after that the replace. if that is to difficult for you than do something else!" << std::endl;
-	return 0;
+		std::cout << "Usage: ./program <filename> <find> <replace>" << std::endl;
+		return 1;
 	}
+	
 	f.setfile(argv[1]);
 	f.setfind(argv[2]);
 	f.setreplace(argv[3]);
-
+	
+	f.makeNewFile();
+	std::string content = f.readFileToString();
+	std::string result = f.replaceInString(content);
+	
+	std::cout << "Replacement done!" << std::endl;
+	
+	return 0;
 }
